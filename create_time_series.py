aab2dbdf3cd48c8ed77771948b7be0f576013c5d
SarahLechler <s_lech05@uni-muenster.de>
2020-10-26 20:42:54 +0100

    fix creation of monthly timeseries for non bap images

--- create_time_series.py
+++ create_time_series.py
@@ -3,7 +3,10 @@ import os
 from osgeo import gdal
 import datetime
 import utils
+import ranking
+
 directoryPath = "./Images"
+
 
 def get_indice_img_paths(indice_name, tile_name):
     filePathArray = []
@@ -20,32 +23,28 @@ def get_indice_img_paths(indice_name, tile_name):
                                 indices_dir_path = os.path.join(dirPath, indices_dir_path)
                                 if os.path.isdir(indices_dir_path):
                                     for file in os.listdir(indices_dir_path):
-                                        if file.find(indice_name) != -1 and file.find(".xml") == -1 and file.find(
-                                                "classified") == -1 and file.find(
-                                                "training_data") == -1:
+                                        if indice_name in file and ".xml" not in file and "classified" not in file and "training_data" not in file:
                                             filePath = os.path.join(indices_dir_path, file)
                                             month = utils.extract_sensing_month_from_filename(filePath)
-                                            if month < 8 and year == 2018 or year != 2018:
+                                            if (month < 7 and year == 2020) or year != 2020 or (
+                                                    month > 7 and year == 2013):
                                                 filePathArray.append(filePath)
-    return filePathArray
+    index_images = ranking.group_images_per_month(filePathArray)
+    monthly_images = []
+    for images in index_images:
+        ranking_results = ranking.create_cloud_ranking(images)
+        monthly_images.append(ranking_results)
+    return monthly_images
 
 
 def get_pixel_value_array(img_array):
     first_data = gdal.Open(img_array[0])
     time_series_array = first_data.ReadAsArray()
-    for img_path in img_array [1:]:
+    for img_path in img_array[1:]:
         data = gdal.Open(img_path)
         data_array = data.ReadAsArray()
-        np.vstack(time_series_array, data_array)
-    """for img_index, img_path in enumerate(img_array, start=0):
-        data = gdal.Open(img_path)
-        data_array = data.ReadAsArray()
-        
-        for lat_index, lat_array in enumerate(data_array, start=0):
-            for long_index, pixel in enumerate(lat_array, start=0):
-                if (lat_index > rows - 1 or long_index > columns - 1):
-                    break
-                time_series_array[img_index][lat_index][long_index] = pixel"""
+        time_series_array = np.dstack((time_series_array, data_array))
+    time_series_array = np.swapaxes(time_series_array, 0, 2)
     return time_series_array
 
 
@@ -74,23 +73,30 @@ input:  index name String (NDVI, RNSDI, SAVI, GEMI EVI)
         portion array with Integers describing the portion of the image
 """
 
+
 def get_indice_bap_img_paths(index, tile, directoryPath):
-    yearly_images = []
+    bap_images = []
+    # make single for loops
     for item in os.listdir(directoryPath):
         tilePath = os.path.join(directoryPath, item)
         if os.path.isdir(tilePath) and item == tile:
             for year_folder in os.listdir(tilePath):
                 year_path = os.path.join(tilePath, year_folder)
-                for index_bap_files in os.listdir(year_path):
-                    if index_bap_files.endswith(".tif") and index in index_bap_files:
-                        index_path = os.path.join(year_path, index_bap_files)
-                        yearly_images.append(index_path)
-    return yearly_images
+                if os.path.isdir(year_path):
+                    for index_bap_files in os.listdir(year_path):
+                        if index_bap_files.endswith(".tif") and index in index_bap_files:
+                            index_path = os.path.join(year_path, index_bap_files)
+                            month = utils.extract_sensing_month_from_filename(index_path)
+                            year = utils.extract_sensing_year_from_filename(index_path)
+                            if (month < 7 and year == 2020) or year != 2020 or (
+                                    month > 7 and year == 2013):
+                                bap_images.append(index_path)
+    return bap_images
 
 
 def create_time_series(name, tile, bap):
     if bap:
-        paths = get_indice_bap_img_paths(name, tile, "hls_dataset")
+        paths = get_indice_bap_img_paths(name, tile, "../../../../scratch/tmp/s_lech05/hls_data/")
     else:
         paths = get_indice_img_paths(name, tile)
     sortedImgPaths = sortImgPahts(paths)
